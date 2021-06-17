37537bc41a280bc6f2eaf62028bb5c711751b3fa
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-06-18 02:06:54 +0430

    add css baseline & IranYekan font and change palette colors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from "@material-ui/core/styles"
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import App from './App'
 import { defaultTheme } from './theme/default'
@@ -9,6 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
       <div dir="rtl">
+        <CssBaseline />
         <App />
       </div>
     </ThemeProvider>
--- frontend/src/theme/default.js
+++ frontend/src/theme/default.js
@@ -1,5 +1,40 @@
-import { createMuiTheme, makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles"
+
+import IRANYekanWebRegular from './fonts/IRANYekanWeb-Regular.woff'
+
+const IRANYekan = {
+    fontFamily: 'IRANYekan',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    src: `
+    url(${IRANYekanWebRegular}) format('woff')
+  `
+}
 
 export const defaultTheme = createMuiTheme({
     direction: 'rtl',
+    palette: {
+        primary: {
+            main: '#c62828',
+        },
+        secondary: {
+            main: '#2196f3',
+        },
+        background: {
+            default: "#ffffff"
+        },
+    },
+    typography: {
+        fontFamily: [
+            'IRANYekan',
+            'Roboto'
+        ].join(','),
+    },
+    overrides: {
+        MuiCssBaseline: {
+            '@global': {
+                '@font-face': [IRANYekan],
+            },
+        },
+    },
 });

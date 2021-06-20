67948500d4c8e05f84d43fae6a26bf95882af186
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-06-20 17:12:44 +0430

    set jss-rtl for completion material ui RTL

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ThemeProvider } from "@material-ui/core/styles"
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { create } from 'jss'
+import rtl from 'jss-rtl'
+
+import { StylesProvider, jssPreset, ThemeProvider } from "@material-ui/core/styles"
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 import App from './App'
 import { defaultTheme } from './theme/default'
 
+// Configure JSS
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <div dir="rtl">
-        <CssBaseline />
-        <App />
-      </div>
-    </ThemeProvider>
+    <StylesProvider jss={jss}>
+      <ThemeProvider theme={defaultTheme}>
+        <div dir="rtl">
+          <CssBaseline />
+          <App />
+        </div>
+      </ThemeProvider>
+    </StylesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )

295683d74ab839a5bc6076fb2fcff29b8a402eac
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-06-25 23:42:17 +0430

    create admin order page with filter orders by status

--- frontend/src/pages/admin/orders/Orders.page.js
+++ frontend/src/pages/admin/orders/Orders.page.js
@@ -1,10 +1,166 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import RouterLink from 'components/RouterLink'
+
 import AdminLayout from 'layout/admin/Admin.layout'
-import React from 'react'
+
+import AdminTable from 'pages/admin/components/AdminTable'
+import EditText from 'pages/admin/components/EditText'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import Button from '@material-ui/core/Button'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Radio from '@material-ui/core/Radio'
+
+import { IoMdCheckbox } from 'react-icons/io'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing(2)
+    },
+    topMenu: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginTop: theme.spacing(2)
+    },
+    spinner: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    image: {
+        border: '1px solid #bdbdbd',
+        width: theme.spacing(12),
+        height: theme.spacing(12)
+    }
+}))
 
 const OrdersPage = () => {
+
+    const classes = useStyles()
+
+    const columns = [
+        {
+            id: 'customer',
+            label: 'نام مشتری'
+        },
+        {
+            id: 'totalPrice',
+            label: 'مجموع قیمت'
+        },
+        {
+            id: 'orderTime',
+            label: 'زمان ثبت سفارش'
+        },
+        {
+            id: 'action',
+            label: 'عملیات'
+        }
+    ]
+
+    const [rows, setRows] = useState([])
+
+    const [totalCount, setTotalCount] = useState(0)
+    const [page, setPage] = useState(0)
+    const [rowsPerPage, setRowsPerPage] = useState(5)
+    const [orderStatus, setOrderStatus] = useState('total')
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setRows([])
+            const res = await axios.get(`/orders?` +
+                (orderStatus === 'waiting' ? 'isDelivery=false&' : orderStatus === 'delivered' ? 'isDelivery=true&' : '') +
+                `_page=${page + 1}&_limit=${rowsPerPage}`)
+
+            res.data.map((item) => {
+                let totalPrice = 0
+                item.orderList.map((order) => {
+                    totalPrice += Number(order.price)
+                })
+
+                let { id, customer, orderTime } = item
+                return setRows(rows => [...rows, { id, customer, totalPrice, orderTime }])
+            })
+            setTotalCount(+res.headers['x-total-count'])
+        }
+        fetchData()
+    }, [orderStatus, page, rowsPerPage])
+
+    const handleChangePage = (e, newPage) => {
+        setPage(newPage);
+    }
+
+    const handleChangeRowsPerPage = (e) => {
+        setRowsPerPage(+e.target.value)
+        setPage(0)
+    }
+
+    const handleOrderStatusChange = (e) => {
+        setOrderStatus(e.target.value)
+        console.log(e.target.value)
+    }
+
     return (
         <AdminLayout>
-            Admin Orders Page
+            <div className={classes.topMenu}>
+                <Typography variant="h6">مدیریت سفارش ها</Typography>
+                <RadioGroup row name="orderStatus" value={orderStatus} onChange={handleOrderStatusChange}>
+                    <FormControlLabel value="total" control={<Radio color="primary" />} label="همه سفارش ها" />
+                    <FormControlLabel value="delivered" control={<Radio color="primary" />} label="سفارش های تحویل شده" />
+                    <FormControlLabel value="waiting" control={<Radio color="primary" />} label="سفارش های در انتظار ارسال" />
+
+                </RadioGroup>
+            </div>
+
+            {rows.length === 0 ? <div className={classes.spinner}><CircularProgress /></div> :
+                <Paper elevation={3} className={classes.root}>
+                    <AdminTable
+                        head={
+                            <TableRow>
+                                {columns.map((column) => (
+                                    <TableCell
+                                        key={column.id}
+                                    >
+                                        {column.label}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        }
+
+                        body={rows.map((row) => (
+                            <TableRow key={row.id}>
+                                <TableCell align="left" style={{ width: "30%" }} size='small'>{row.customer}</TableCell>
+                                <TableCell align="left" style={{ width: "25%" }}>{row.totalPrice}</TableCell>
+                                <TableCell align="left" style={{ width: "25%" }}>{row.orderTime}</TableCell>
+                                <TableCell align="left" style={{ width: "20%" }}>
+                                    <Button
+                                        color="secondary"
+                                        startIcon={<IoMdCheckbox />}
+                                    >بررسی سفارش</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+
+                        pagination={
+                            {
+                                rowsPerPageOptions: [5, 10, 15, 25],
+                                totalCount,
+                                page,
+                                rowsPerPage,
+                                handleChangePage,
+                                handleChangeRowsPerPage
+                            }
+                        }
+
+                    />
+                </Paper>
+            }
         </AdminLayout>
     )
 }

b6a9aecff9734d5b5f7d1478ef78d96367f7ec4d
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-06-27 15:08:24 +0430

    providing the redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,21 @@ import ShopRoute from 'routes/Shop.route'
 import AdminRoute from 'routes/Admin.route'
 import NotFoundPage from 'pages/NotFound.page'
 
+//Redux
+import { Provider } from 'react-redux'
+import store from 'redux/store'
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopRoute} />
-        <Route path='/admin' component={AdminRoute} />
-        <Route component={NotFoundPage}></Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopRoute} />
+          <Route path='/admin' component={AdminRoute} />
+          <Route component={NotFoundPage}></Route>
+        </Switch>
+      </Router>
+    </Provider>
   )
 }
 

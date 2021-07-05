207ef326d1e595da17012c52d73395a81fe64077
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-07-06 01:46:51 +0430

    create form validation

---
+++ frontend/src/utils/formValidation.js
@@ -0,0 +1,14 @@
+export const productValidation = (values, formData, errors, setErrors) => {
+    const temp = { ...errors }
+    if ('name' in values)
+        temp.name = values.name ? '' : 'نام محصول اجباری می باشد'
+    if ('description' in values)
+        temp.description = values.description ? '' : 'توضیحات محصول اجباری می باشد'
+    if ('categories' in values)
+        temp.categories = values.categories.length !== 0 ? '' : 'لطفا یک دسته بندی انتخاب کنید'
+
+    setErrors({ ...temp })
+
+    if (values == formData)
+        return Object.values(temp).every(x => x == '')
+}

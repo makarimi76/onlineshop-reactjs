3d7ada664bed417d8928ccf33fd1c322d000869a
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-07-08 22:25:38 +0430

    fix admins order and category reducer

--- frontend/src/redux/reducers/admin/order.reducer.js
+++ frontend/src/redux/reducers/admin/order.reducer.js
@@ -11,7 +11,7 @@ const initialState = {
     error: null
 }
 
-export default function product(state = initialState, action) {
+export default function order(state = initialState, action) {
     const { type, payload } = action
 
     switch (type) {

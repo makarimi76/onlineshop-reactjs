fdbb92dec95d71492a4ddbe043a414fb79c6b0ac
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-07-09 01:26:35 +0430

    show products in products page

--- frontend/src/pages/shop/products/Products.page.js
+++ frontend/src/pages/shop/products/Products.page.js
@@ -1,31 +1,74 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
 // Redux
 import { getCategories } from 'redux/actions/shop/category.action'
+import { getProductsByCategory, startProductLoading } from 'redux/actions/shop/product.action'
 
 //Componnets
 import ProductsLayout from 'layout/shop/Products.layout'
 import Categories from 'pages/shop/products/components/Categories'
+import ProductItem from 'pages/shop/components/ProductsItem'
 
-const ProductsPage = ({ match, category: { categories, loading }, getCategories }) => {
+// UI
+import { makeStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 
+const useStyles = makeStyles((theme) => ({
+    spinner: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    box: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(2)
+    }
+}))
+
+const ProductsPage = ({ match, category: { categories }, product: { products, loading }, getCategories, getProductsByCategory, startProductLoading }) => {
+
+    const classes = useStyles()
+
+    const [category, setCategoty] = useState(null)
     useEffect(() => {
         if (categories.length === 0)
             getCategories()
     }, [getCategories, categories])
 
+
+    useEffect(() => {
+        const findCategory = categories.find(item => item.slug === match.params.category)
+        setCategoty(findCategory)
+        if (categories.length !== 0) {
+            startProductLoading()
+            getProductsByCategory(findCategory.name, 1, 10)
+        }
+    }, [match, categories])
+
     return (
         <ProductsLayout side={<Categories />}>
-            {match.params.category} Products Page
+            {loading ? <div className={classes.spinner}><CircularProgress /></div> :
+                [<Typography variant="h5">کالا های گروه {category.name.replace('کالاهای', '')}</Typography>,
+                <Grid container spacing={3} className={classes.box}>
+                    {products.map(item => (
+                        <Grid key={item.id} item xs={12} sm={6} md={6} lg={4} >
+                            <ProductItem product={item} />
+                        </Grid>
+                    ))}
+                </Grid>
+                ]
+            }
         </ProductsLayout>
     )
 }
 
 const mapStateToProps = ({ shop }) => ({
-    category: shop.category
+    category: shop.category,
+    product: shop.product
 })
 
 export default connect(mapStateToProps, {
-    getCategories
+    getCategories, getProductsByCategory, startProductLoading
 })(ProductsPage)

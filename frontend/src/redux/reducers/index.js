c6d0894c60a5d7949f7e42649682886ea333171c
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-07-08 19:28:19 +0430

    shop redux with chategories

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux"
 
 import admin from 'redux/reducers/admin'
+import shop from 'redux/reducers/shop'
 import alert from 'redux/reducers/alert.reducer'
 
 export default combineReducers({
     admin,
+    shop,
     alert
 })
---
+++ frontend/src/redux/reducers/shop/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux"
+
+import category from 'redux/reducers/shop/category.reducer'
+
+export default combineReducers({
+    category
+})

53a643bd019d932a7ae9a8fd2ba088034615fc08
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-07-14 03:52:02 +0430

    show error for no cart and create bottom menu

--- frontend/src/pages/shop/Cart.page.js
+++ frontend/src/pages/shop/Cart.page.js
@@ -12,6 +12,7 @@ import Paper from '@material-ui/core/Paper'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
 import { IoMdTrash } from 'react-icons/io'
 
@@ -19,6 +20,23 @@ const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
         marginTop: theme.spacing(4)
+    },
+    noCart: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: theme.spacing(8)
+    },
+    bottomMenu: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginTop: theme.spacing(2)
+    }, checkout: {
+        color: '#fff',
+        backgroundColor: theme.palette.success.main,
+        '&:hover': {
+            backgroundColor: theme.palette.success.dark,
+        }
     }
 }))
 
@@ -53,36 +71,51 @@ const CartPage = ({ cart: { carts }, removeCart }) => {
 
     return (
         <ShopLayout maxWidth='md' pageTitle="سبد خرید">
-            <Paper elevation={3} className={classes.root}>
-                <Table
-                    head={
-                        <TableRow>
-                            {columns.map((column) => (
-                                <TableCell
-                                    key={column.id}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    }
+            {carts.length === 0 ? (
+                <div className={classes.noCart}><Typography variant="h6" color="error" align="center">سبد خرید شما خالی می باشد</Typography></div>
+            ) :
+                <>
+                    <Paper elevation={3} className={classes.root}>
+                        <Table
+                            head={
+                                <TableRow>
+                                    {columns.map((column) => (
+                                        <TableCell
+                                            key={column.id}
+                                        >
+                                            {column.label}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            }
 
-                    body={carts.map((row) => (
-                        <TableRow key={row.id}>
-                            <TableCell align="left" style={{ width: "50%" }} size='small'>{row.name}</TableCell>
-                            <TableCell align="left" style={{ width: "20%" }}>{row.price}</TableCell>
-                            <TableCell align="left" style={{ width: "15%" }}>{row.quantity}</TableCell>
-                            <TableCell align="left" style={{ width: "15%" }}>
-                                <Button
-                                    color="primary"
-                                    startIcon={<IoMdTrash />}
-                                    onClick={() => handleRemoveClick(row.id)}
-                                >حذف</Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                />
-            </Paper>
+                            body={carts.map((row) => (
+                                <TableRow key={row.id}>
+                                    <TableCell align="left" style={{ width: "50%" }} size='small'>{row.name}</TableCell>
+                                    <TableCell align="left" style={{ width: "20%" }}>{row.price}</TableCell>
+                                    <TableCell align="left" style={{ width: "15%" }}>{row.quantity}</TableCell>
+                                    <TableCell align="left" style={{ width: "15%" }}>
+                                        <Button
+                                            color="primary"
+                                            startIcon={<IoMdTrash />}
+                                            onClick={() => handleRemoveClick(row.id)}
+                                        >حذف</Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        />
+                    </Paper>
+
+                    <div className={classes.bottomMenu}>
+                        <Typography variant="h6">جمع: {carts.map(cart => cart.price * cart.quantity).reduce((a, b) => a + b, 0)} تومان</Typography>
+                        <Button
+                            className={classes.checkout}
+                            variant="contained"
+                            size="large"
+                        >نهایی کردن سبد خرید</Button>
+                    </div>
+                </>
+            }
         </ShopLayout>
     )
 }
